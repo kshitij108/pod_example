7342962e53d99e3da9ae9b105ed3019fbb5a521e
kshitij108 <kshitij108@gmail.com>
2014-05-12 21:35:39 -0400

    first commit for pod example

---
+++ testPod/KBViewController.h
@@ -0,0 +1,13 @@
+//
+//  KBViewController.h
+//  testPod
+//
+//  Created by Kshitij on 2014-05-05.
+//  Copyright (c) 2014 Kshitij. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBViewController : UIViewController
+
+@end
---
+++ testPod/KBViewController.m
@@ -0,0 +1,29 @@
+//
+//  KBViewController.m
+//  testPod
+//
+//  Created by Kshitij on 2014-05-05.
+//  Copyright (c) 2014 Kshitij. All rights reserved.
+//
+
+#import "KBViewController.h"
+
+@interface KBViewController ()
+
+@end
+
+@implementation KBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
